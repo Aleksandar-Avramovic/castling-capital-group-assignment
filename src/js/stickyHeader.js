07e2e0f98eddeab2470b4c35e333f4337c9ed8ed
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-06-05 16:59:55 +0200

    Console.log() deleted

--- src/js/stickyHeader.js
+++ src/js/stickyHeader.js
@@ -1,5 +1,4 @@
 window.addEventListener("scroll", function () {
   const header = document.querySelector(".header");
-  console.log(header);
   header.classList.toggle("header--sticky", window.scrollY > 0);
 });
