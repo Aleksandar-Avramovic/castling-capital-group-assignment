854d52f29de158b537076a8d2e6d64ef5dff17e0
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-06-07 16:45:07 +0200

    Added redirections on links with smooth scrolling

---
+++ src/js/smoothScrolling.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  for (let link of links) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      window.scrollTo({
+        top: targetElement.offsetTop - 86,
+        behavior: "smooth",
+      });
+    });
+  }
+});
