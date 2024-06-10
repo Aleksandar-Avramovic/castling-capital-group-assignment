2c7e9e86a0341b6f913d6517241ef63b6a4d0694
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-06-10 15:12:35 +0200

    Mobile menu changed

--- src/js/stickyHeader.js
+++ src/js/stickyHeader.js
@@ -1,13 +1,43 @@
+const header = document.querySelector(".header");
+const headerLogo = document.querySelector(".header__logo");
+const links = document.querySelectorAll(".header__nav ul li");
+
+window.addEventListener("load", function () {
+  if (!header.classList.contains("header-sticky")) {
+    startAnimations();
+  }
+});
+
 window.addEventListener("scroll", function () {
-  const header = document.querySelector(".header");
-  const headerLogo = document.querySelector(".header__logo");
   header.classList.toggle("header--sticky", window.scrollY > 0);
 
   if (header.classList.contains("header--sticky")) {
+    stopAnimations();
     header.style.height = "56px";
     headerLogo.style.width = "80%";
   } else {
     header.style.height = "86px";
+    header.style.transition = "0.3s";
     headerLogo.style.width = "100%";
+    headerLogo.style.transition = "0.3s";
   }
 });
+
+function startAnimations() {
+  links.forEach((link, index) => {
+    link.style.animation = "fall 1s cubic-bezier(0.22, 0.61, 0.36, 1) forwards";
+    link.style.animationDelay = index * 0.2 + "s";
+  });
+
+  headerLogo.style.animation = "zoomIn 1s ease-out forwards";
+}
+
+function stopAnimations() {
+  links.forEach((link) => {
+    link.style.animation = "none";
+    link.style.transform = "translateY(0)";
+    link.style.opacity = "1";
+  });
+  headerLogo.style.animation = "none";
+  headerLogo.style.transform = "scale(1)";
+}
