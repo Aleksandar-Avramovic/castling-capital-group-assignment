273014048de0ffb7b4626956eb24b1f684788dad
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-06-18 14:41:26 +0200

    Added email and phone format validation on form

--- src/js/formValidation.js
+++ src/js/formValidation.js
@@ -1,59 +1,67 @@
-const validateName = () => {
-  const name = document.getElementById("name").value;
-  const nameMsg = document.getElementById("name-msg");
+// Input field validator
+const validateField = (fieldId, msgId) => {
+  const value = document.getElementById(fieldId).value;
+  const msgElement = document.getElementById(msgId);
 
-  nameMsg.textContent = "";
-  nameMsg.classList.remove("shown");
+  msgElement.textContent = "";
+  msgElement.classList.remove("shown");
 
-  if (!name) {
-    nameMsg.classList.add("shown");
-    nameMsg.textContent = "The field is required";
+  if (!value) {
+    msgElement.classList.add("shown");
+    msgElement.textContent = "The field is required";
     return false;
   }
   return true;
 };
 
+// Email field validator
 const validateEmail = () => {
   const email = document.getElementById("email").value;
   const emailMsg = document.getElementById("email-msg");
-
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   emailMsg.textContent = "";
+  emailMsg.classList.remove("shown");
 
   if (!email) {
     emailMsg.classList.add("shown");
     emailMsg.textContent = "The field is required";
     return false;
   }
+
+  if (!emailRegex.test(email)) {
+    emailMsg.classList.add("shown");
+    emailMsg.textContent = "E-mail is invalid";
+    return false;
+  }
   return true;
 };
 
+// Phone field validator
 const validatePhone = () => {
   const phone = document.getElementById("phone").value;
   const phoneMsg = document.getElementById("phone-msg");
 
   phoneMsg.textContent = "";
+  phoneMsg.classList.remove("shown");
 
   if (!phone) {
     phoneMsg.classList.add("shown");
     phoneMsg.textContent = "The field is required";
     return false;
   }
-  return true;
-};
 
-const validateMessage = () => {
-  const message = document.getElementById("message").value;
-  const messageMsg = document.getElementById("message-msg");
-
-  messageMsg.textContent = "";
-
-  if (!message) {
-    messageMsg.classList.add("shown");
-    messageMsg.textContent = "The field is required";
+  const phoneRegex = /^\d{3}\s?\d{3}\s?\d{4}$/;
+  if (!phoneRegex.test(phone)) {
+    phoneMsg.classList.add("shown");
+    phoneMsg.textContent = "Phone number is invalid";
     return false;
   }
+
   return true;
 };
+
+const validateName = () => validateField("name", "name-msg");
+const validateMessage = () => validateField("message", "message-msg");
 
 // Validate form on submit
 document.getElementById("form").addEventListener("submit", (e) => {
@@ -69,8 +77,14 @@ document.getElementById("form").addEventListener("submit", (e) => {
   }
 });
 
-// Validate fields on change
-document.getElementById("name").addEventListener("change", validateName);
-document.getElementById("email").addEventListener("change", validateEmail);
-document.getElementById("phone").addEventListener("change", validatePhone);
-document.getElementById("message").addEventListener("change", validateMessage);
+// Validate form on change and focusout
+const addValidationListeners = (fieldId, validationFunction) => {
+  const field = document.getElementById(fieldId);
+  field.addEventListener("change", validationFunction);
+  field.addEventListener("focusout", validationFunction);
+};
+
+addValidationListeners("name", validateName);
+addValidationListeners("email", validateEmail);
+addValidationListeners("phone", validatePhone);
+addValidationListeners("message", validateMessage);
