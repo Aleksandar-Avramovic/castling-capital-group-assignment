7bdcaf09567d0c7cd264ddbf201322a4f6f4b947
Aleksandar Avramovic <aleksandar.avramovic@forwardslashny.com>
2024-06-19 13:26:58 +0200

    Fixed header animations

--- src/js/stickyHeader.js
+++ src/js/stickyHeader.js
@@ -1,52 +1,18 @@
-const header = document.querySelector(".header");
-const headerLogo = document.querySelector(".header__logo");
-const links = document.querySelectorAll(".header__nav ul li");
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".header");
+  const headerLogo = document.querySelector(".header__logo");
+  const links = document.querySelectorAll(".header__nav ul li");
 
-// window.addEventListener("load", function () {
-//   if (
-//     !header.classList.contains("header--sticky") ||
-//     !window.innerWidth <= 768
-//   ) {
-//     startAnimations();
-//   } else {
-//     stopAnimations();
-//   }
-// });
-
-window.addEventListener("scroll", function () {
-  header.classList.toggle("header--sticky", window.scrollY > 0);
-
-  // if (header.classList.contains("header--sticky") || window.innerWidth <= 768) {
-  //   stopAnimations();
-  //   header.style.height = "66px";
-  //   headerLogo.style.width = "80%";
-  // } else {
-  //   header.style.height = "86px";
-  //   header.style.transition = "0.3s";
-  //   headerLogo.style.width = "100%";
-  //   headerLogo.style.transition = "0.3s";
-  // }
+  header.classList.add("loaded");
+  window.addEventListener("scroll", function () {
+    header.classList.toggle("header--sticky", window.scrollY > 0);
+    header.classList.remove("loaded");
+    headerLogo.style.transform = "scale(1)";
+    headerLogo.style.transition = "all 0.35s ease";
+    links.forEach((link) => {
+      link.style.animation = "none";
+      link.style.opacity = "1";
+      link.style.transform = "translateY(0)";
+    });
+  });
 });
-
-// function startAnimations() {
-//   links.forEach((link, index) => {
-//     link.style.animation = "fall 1s cubic-bezier(0.22, 0.61, 0.36, 1) forwards";
-//     link.style.animationDelay = index * 0.2 + "s";
-//   });
-
-//   if (!window.innerWidth <= 768) {
-//     headerLogo.style.animation = "zoomIn 1s ease-out forwards";
-//   } else {
-//     stopAnimations();
-//   }
-// }
-
-// function stopAnimations() {
-//   links.forEach((link) => {
-//     link.style.animation = "none";
-//     link.style.transform = "translateY(0)";
-//     link.style.opacity = "1";
-//   });
-//   headerLogo.style.animation = "none";
-//   headerLogo.style.transform = "scale(1)";
-// }
